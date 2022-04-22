75fdb84c6e9e82a571be8afcbe6ef3177ac5e48d
Aidan Border <planes4u@outlook.com>
2022-04-22 11:51:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/* Aidan Border
+*
+* ALGORITHM:
+* MergeSort(Array[0...n-1])
+* input: An unsorted array
+* Output: A sorted copy of the input array
+* Constraints:
+*
+*
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
